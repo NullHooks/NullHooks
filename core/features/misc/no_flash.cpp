61a0a31c8c084dc850b1ac845c8f870f44f5292b
ravi0li <mikel.veg777@gmail.com>
2021-07-05 15:26:38 +0200

    v1.3
    Better noflash because now you can disable it.

--- core/features/misc/no_flash.cpp
+++ core/features/misc/no_flash.cpp
@@ -1,16 +1,41 @@
 #include "../features.hpp"
 
 void visuals::noflash::handle() {
-
 	if (!variables::noflash_bool)
+	{
+		if (!interfaces::engine->is_connected() && !interfaces::engine->is_in_game())
+			return;
+
+		if (!csgo::local_player)
+			return;
+
+		csgo::local_player->flash_alpha() = 255.f;
+	}
+	else
+	{
+		if (!interfaces::engine->is_connected() && !interfaces::engine->is_in_game())
+			return;
+
+		if (!csgo::local_player)
+			return;
+
+		if (csgo::local_player->flash_alpha() > 0.f)
+			csgo::local_player->flash_alpha() = 0.f;
+	}	
+}
+
+/*
+void visuals::noflash::handle() {
+
+	if (!variables::noflash_bool) {
 		return;
+		csgo::local_player->flash_duration() = 255.0f;
+		csgo::local_player->flash_alpha() = 255.0f;
+	}
 
 	if (variables::noflash_bool) {
 		csgo::local_player->flash_duration() = 0.0f;
 		csgo::local_player->flash_alpha() = 0.0f;
 	}
-	else {
-		csgo::local_player->flash_duration() = 255.0f;
-		csgo::local_player->flash_alpha() = 255.0f;
-	}
 }
+*/
