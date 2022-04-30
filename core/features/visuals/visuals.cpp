aa6eb19a13806b3c737df64a62c058f8f8e42b97
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-04-30 18:41:51 +0200

    Move box esp and name esp to the same function for optimization

--- core/features/visuals/visuals.cpp
+++ core/features/visuals/visuals.cpp
@@ -1,22 +1,17 @@
 #include "../features.hpp"
 
-void visuals::boxesp()
-{
-	if (!variables::boxesp) return;
+void visuals::playeresp() {
+	if (!(variables::boxesp_bool || variables::nameesp_bool)) return;
 	if (!interfaces::engine->is_connected() || !interfaces::engine->is_in_game()) return;
 	if (!csgo::local_player) return;
 
 	for (int iPlayer = 0; iPlayer < interfaces::globals->max_clients; iPlayer++)
 	{
 		auto pCSPlayer = reinterpret_cast<player_t*>(interfaces::entity_list->get_client_entity(iPlayer));
-		if (!pCSPlayer)
-			continue;
-		if (pCSPlayer == csgo::local_player)
-			continue;
-		if (pCSPlayer->dormant())
-			continue;
-		if (!(pCSPlayer->is_alive() && pCSPlayer->health() > 0))
-			continue;
+		if (!pCSPlayer) continue;
+		if (pCSPlayer == csgo::local_player) continue;
+		if (pCSPlayer->dormant()) continue;
+		if (!(pCSPlayer->is_alive() && pCSPlayer->health() > 0)) continue;
 
 		vec3_t vecFoot, vecScreen, vecHeadScreen;
 		vecFoot = pCSPlayer->origin();
@@ -25,25 +20,33 @@ void visuals::boxesp()
 		vecFoot.z += 72.f;
 		if (!(math::world_to_screen(vecFoot, vecHeadScreen))) continue;
 
-		int h = vecScreen.y - vecHeadScreen.y;
-		int w = (h / 5) * 2;
-		int y = vecHeadScreen.y;
-		int x = vecHeadScreen.x - w / 2;
-		if (pCSPlayer->team() == csgo::local_player->team() && variables::showteamesp)
-		{
-			render::draw_rect(x, y, w, h, color::blue()); // drawing with render tools
-			//interfaces::surface->set_drawing_color(0, 0, 255, 255);  second way of drawing it with the surface interface, we set color, blue here
-			//interfaces::surface->draw_outlined_rect(x, y, w, h);  then we draw
+		const int h = vecScreen.y - vecHeadScreen.y;
+		const int w = (h / 5) * 2;
+		const int y = vecHeadScreen.y;
+		const int x = vecHeadScreen.x - w / 2;
+
+		if (variables::boxesp_bool) {
+			if (pCSPlayer->team() == csgo::local_player->team() && variables::showteamesp_bool)
+				render::draw_rect(x, y, w, h, color::blue()); // drawing with render tools
+				//interfaces::surface->set_drawing_color(0, 0, 255, 255);  second way of drawing it with the surface interface, we set color, blue here
+				//interfaces::surface->draw_outlined_rect(x, y, w, h);  then we draw
+			else if (pCSPlayer->team() != csgo::local_player->team())
+				render::draw_rect(x, y, w, h, color::red());
 		}
-		else if(pCSPlayer->team() != csgo::local_player->team())
-		{
-			render::draw_rect(x, y, w, h, color::red()); // drawing with render tools
-			//interfaces::surface->set_drawing_color(255, 0, 0, 255);  second way of drawing it with the surface interface, we set color, red here
-			//interfaces::surface->draw_outlined_rect(x, y, w, h);  then we draw
+
+		if (variables::nameesp_bool) {
+			player_info_t playerinfo;
+			interfaces::engine->get_player_info(iPlayer, &playerinfo);
+
+			if (pCSPlayer->team() == csgo::local_player->team() && variables::showteamesp_bool)
+				render::draw_text_string(x + 5, y + h + 2, render::fonts::watermark_font, playerinfo.name, true, color::blue());
+			else if (pCSPlayer->team() != csgo::local_player->team())
+				render::draw_text_string(x + 5, y + h + 2, render::fonts::watermark_font, playerinfo.name, true, color::red());
 		}
 	}
 }
 
+/*
 void visuals::nameesp()
 {
 	if (!variables::nameesp_bool) return;
@@ -72,6 +75,7 @@ void visuals::nameesp()
 			render::draw_text_string(vecHeadScreen.x, vecHeadScreen.y, render::fonts::watermark_font, playerinfo.name, true, color::red());
 	}
 }
+*/
 
 // ------------------------------
 
--- core/menu/variables.hpp
+++ core/menu/variables.hpp
@@ -9,14 +9,12 @@ namespace variables {
 	inline bool aimbot_isvisiblecheck = true;
 
 	// Visuals
-	inline bool showteamesp = false;
-	inline bool boxesp = false;
+	inline bool showteamesp_bool = false;
+	inline bool boxesp_bool = false;
 	inline bool nameesp_bool = false;
 	
 	inline bool noflash_bool  = true;
-	inline bool draw_stats_bool = false;
-	//inline bool draw_stats_bool = false;
-	
+
 	// Misc
 	inline bool bhop_bool = true;
 	inline bool crosshair_bool = false;
