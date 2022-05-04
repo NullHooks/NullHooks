05ff1bd30db46b8884d84aedbf69842b43321733
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-05-04 22:43:44 +0200

    Fix chicken glow (very important)

--- core/helpers/misc_helpers.hpp
+++ core/helpers/misc_helpers.hpp
@@ -5,4 +5,8 @@ namespace custom_helpers {
 	void print_to_console(const char* text);
 	void state_to_console(const char* tag, const char* text);
 	void state_to_console_color(const char* tag, const char* text);
-}
+	color hsv2color(float H, float S, float V);
+	color hsv_float2color(float hue, float saturation, float brightness);
+
+	const float rainbow_speed = 0.001f;
+};
