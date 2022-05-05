471668ea2d4116cd3ddd28443fdc7a4ea2faab5b
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-05-05 17:42:14 +0200

    Add bomb timer line

--- core/features/visuals/entity_info.cpp
+++ core/features/visuals/entity_info.cpp
@@ -12,7 +12,13 @@ void visuals::entity_info::bomb(entity_t* bomb_ent) {
 	float exp_time = flblow - (csgo::local_player->get_tick_base() * interfaces::globals->interval_per_tick);
 	
 	if (exp_time > 0 && !bomb_p->m_bBombDefused()) {
-		// TODO: Progress bar
+		const int bar_w = 600;
+		int screen_width, screen_height;
+		interfaces::engine->get_screen_size(screen_width, screen_height);
+		// Bar
+		render::draw_rect(screen_width / 2 - bar_w / 2, 85, bar_w, 4, color::black(255));
+		render::draw_filled_rect(screen_width / 2 - bar_w / 2 + 1, 84, (40.f - exp_time) / 40.f * bar_w, 4, color(255, 140, 0, 255));	// Assume bomb is always 40s
+		// Timer
 		custom_helpers::draw_bomb_text(exp_time);
 		if (math::world_to_screen(bomb_p->origin(), entPosScreen))
 			render::draw_text_string(entPosScreen.x, entPosScreen.y, render::fonts::watermark_font, "Bomb", true, color(255, 140, 0, 255));
