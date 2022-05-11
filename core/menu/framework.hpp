39acb408584288e67a713b3380bbd560c0b2b388
volkov <andrey.volkov.main@gmail.com>
2022-05-12 01:08:12 +0300

    better button

--- core/menu/framework.hpp
+++ core/menu/framework.hpp
@@ -1,12 +1,13 @@
 #pragma once
 #include "../../dependencies/utilities/csgo.hpp"
 
-namespace menu_framework {
+namespace gui {
 	void group_box(std::int32_t x, std::int32_t y, std::int32_t w, std::int32_t h, unsigned long font, const std::string string, bool show_label);
 	void tab(std::int32_t x, std::int32_t y, std::int32_t w, std::int32_t h, unsigned long font, const std::string, std::int32_t& tab, std::int32_t count);
 	void check_box(std::int32_t x, std::int32_t y, std::int32_t position, unsigned long font, const std::string string, bool& value);
 	void slider(std::int32_t x, std::int32_t y, std::int32_t slider_pos_x, std::int32_t slider_len, unsigned long font, const std::string string, float& value, float min_value, float max_value);
 	void menu_movement(std::int32_t& x, std::int32_t& y, std::int32_t w, std::int32_t h);
+	bool button(std::int32_t x, std::int32_t y, std::int32_t position, unsigned long font, const std::string string);
 
 	inline bool should_drag = false;
 	inline bool should_move = false; 
@@ -16,13 +17,4 @@ namespace spectator_framework {
 	void spec_list_movement(std::int32_t& x, std::int32_t& y, std::int32_t w, std::int32_t h);
 	inline bool should_drag = false;
 	inline bool should_move = false;
-};
-
-namespace custom_menu_framework {
-	void button(std::int32_t x, std::int32_t y, std::int32_t position, unsigned long font, const std::string string, int function_id);
-	void unhook_button(std::int32_t x, std::int32_t y, std::int32_t position, unsigned long font, const std::string string);
-};
-
-namespace menu_button_fuctions {
-	void exec_button_function(int id);
 };
