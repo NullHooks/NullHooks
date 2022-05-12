636b07e1bf0418c43c1e1f5210413d4fffe1085c
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-05-12 17:14:22 +0200

    Revert viewmodel and fov changes

--- core/hooks/functions/get_viewmodel_fov.cpp
+++ core/hooks/functions/get_viewmodel_fov.cpp
@@ -2,7 +2,7 @@
 
 float __fastcall hooks::get_viewmodel_fov::hook(uintptr_t, uintptr_t) {
 	if (interfaces::engine->is_taking_screenshot())
-		return original(interfaces::clientmode);
+		return original(interfaces::clientmode);		// Return original viewmodel fov if in screenshot (68.f)
 
-	return original(interfaces::clientmode) + variables::custom_vmfov_slider;
+	return original(interfaces::clientmode) * variables::custom_vmfov_slider;
 }
