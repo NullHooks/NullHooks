ae5e276795ef7f2011837ce34a5df78394d6301d
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-05-24 18:15:12 +0200

    Format no_flash.cpp

--- src/core/features/visuals/no_flash.cpp
+++ src/core/features/visuals/no_flash.cpp
@@ -7,13 +7,10 @@ void visuals::noflash() {
 	if (!variables::noflash_bool) {
 		if (csgo::local_player->flash_alpha() < 255.f)
 			csgo::local_player->flash_alpha() = 255.f;
-
 		return;
-	} else {
-		if (!interfaces::engine->is_connected() && !interfaces::engine->is_in_game()) return;
-		if (!csgo::local_player) return;
-
-		if (csgo::local_player->flash_alpha() > 0.f)
-			csgo::local_player->flash_alpha() = 0.f;
 	}
+
+	// TODO: Add slider for intensity?
+	if (csgo::local_player->flash_alpha() > 0.f)
+		csgo::local_player->flash_alpha() = 0.f;
 }
