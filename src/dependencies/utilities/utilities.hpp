2b98b79efc2d0d3288c659bc107ae3f7bbb7261c
andrey volkov <90531950+volkov-andrey@users.noreply.github.com>
2022-05-30 01:40:05 +0300

    Update utilities.hpp

--- src/dependencies/utilities/utilities.hpp
+++ src/dependencies/utilities/utilities.hpp
@@ -3,12 +3,5 @@
 #include "../../source-sdk/math/vector3d.hpp"
 
 namespace utilities {
-	template< typename t >
-	__forceinline static t call_virtual( void *name, int offset ) {
-		int *table = *( int ** ) name;
-		int address = table[ offset ];
-		return ( t ) ( address );
-	}
-
 	std::uint8_t* pattern_scan(const char* module_name, const char* signature) noexcept;
 }
