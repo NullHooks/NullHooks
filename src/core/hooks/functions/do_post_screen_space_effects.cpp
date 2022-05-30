9946dc7f156a1cee90877b309977032457a6f48b
andrey volkov <90531950+volkov-andrey@users.noreply.github.com>
2022-05-30 15:20:13 +0300

    Update do_post_screen_space_effects.cpp

--- src/core/hooks/functions/do_post_screen_space_effects.cpp
+++ src/core/hooks/functions/do_post_screen_space_effects.cpp
@@ -1,7 +1,8 @@
 #include "../hooks.hpp"
 
 void __stdcall hooks::do_post_screen_space_effects::hook(const void* viewSetup) {
-	visuals::glow::draw_glow();
+	if (!(interfaces::engine->is_taking_screenshot() && variables::misc::clean_screenshots))
+		visuals::glow::draw_glow();
 
 	original(interfaces::clientmode, viewSetup);
 }
