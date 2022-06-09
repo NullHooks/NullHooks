326c9ba4f87a09332e003d270ae222a54b1aee0f
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-06-09 22:31:40 +0200

    Format find_mdl hook and move draw_models to separate function

--- src/core/hooks/functions/find_mdl.cpp
+++ src/core/hooks/functions/find_mdl.cpp
@@ -10,11 +10,7 @@
  */
 
 unsigned long __stdcall hooks::findmdl::hook(const char* path) noexcept {
-	if (strstr(path, "knife_default_ct.mdl"))                                                       // What model are wen changing
-		return findmdl::original(interfaces::mdl_cache, "models/weapons/v_minecraft_pickaxe.mdl");  // The model we want to replace it with
-	else if (strstr(path, "knife_default_t.mdl"))
-		return findmdl::original(interfaces::mdl_cache, "models/weapons/v_minecraft_pickaxe.mdl");
+	visuals::models::draw_models(path);
 
 	return findmdl::original(interfaces::mdl_cache, path);
 }
-
