b52c8a93512d498a8d6d0f8ca49607e9e2017bd1
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-06-09 23:40:03 +0200

    Move findmdl comment to models.cpp

--- src/core/features/visuals/models.cpp
+++ src/core/features/visuals/models.cpp
@@ -1,5 +1,14 @@
 #include "../features.hpp"
 
+/*
+ * The files should be placed in:
+ *   - C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive
+ *     (Assuming you are on windows and that's where your cs:go is installed)
+ *
+ * Models:
+ *   - Pickaxe: http://gamer-lab.com/home/hl-lab/var/mdl_source/2020/09_5/Iron_Pickaxe/__obj/p_iron_pickaxe.zip
+ */
+
 void visuals::models::draw_models(std::string &path) {
     if (strstr(path.c_str(), "knife_default_ct.mdl") || strstr(path.c_str(), "knife_default_t.mdl"))    // What model are wen changing
         path = "models/weapons/v_minecraft_pickaxe.mdl";                                // The model we want to replace it with
--- src/core/hooks/functions/find_mdl.cpp
+++ src/core/hooks/functions/find_mdl.cpp
@@ -1,14 +1,5 @@
 #include "../hooks.hpp"
 
-/*
- * The files should be placed in:
- *   - C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive
- *     (Assuming you are on windows and that's where your cs:go is installed)
- *
- * Models:
- *   - Pickaxe: http://gamer-lab.com/home/hl-lab/var/mdl_source/2020/09_5/Iron_Pickaxe/__obj/p_iron_pickaxe.zip
- */
-
 unsigned long __stdcall hooks::findmdl::hook(const char* path) noexcept {
 	std::string new_path = path;		// We will change (or not) the path in draw_models()
 
