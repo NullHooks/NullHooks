ac07752410295bec3017658cc92587f17e577cca
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-06-09 21:42:47 +0200

    Format code and add comments for models and installation

--- src/core/hooks/functions/find_mdl.cpp
+++ src/core/hooks/functions/find_mdl.cpp
@@ -1,10 +1,20 @@
 #include "../hooks.hpp"
 
-unsigned long __stdcall hooks::findmdl::hook(const char* path) noexcept
-{
-	if (strstr(path, "knife_default_ct.mdl")) // to what model change
-		return findmdl::original(interfaces::mdl_cache, "models/weapons/v_minecraft_pickaxe.mdl"); // model
+/*
+ * The files should be placed in:
+ *   - C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive
+ *     (Assuming you are on windows and that's where your cs:go is installed)
+ *
+ * Models:
+ *   - Pickaxe: http://gamer-lab.com/home/hl-lab/var/mdl_source/2020/09_5/Iron_Pickaxe/__obj/p_iron_pickaxe.zip
+ */
+
+unsigned long __stdcall hooks::findmdl::hook(const char* path) noexcept {
+	if (strstr(path, "knife_default_ct.mdl"))                                                       // What model are wen changing
+		return findmdl::original(interfaces::mdl_cache, "models/weapons/v_minecraft_pickaxe.mdl");  // The model we want to replace it with
 	else if (strstr(path, "knife_default_t.mdl"))
 		return findmdl::original(interfaces::mdl_cache, "models/weapons/v_minecraft_pickaxe.mdl");
+
 	return findmdl::original(interfaces::mdl_cache, path);
 }
+
