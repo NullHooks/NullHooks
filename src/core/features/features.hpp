4974c974d5ff4c8147b58317dc6dc3baed143a81
Null <29655971+r4v10l1@users.noreply.github.com>
2022-06-09 22:08:56 +0200

    Remove semicolons from namespaces in features.hpp

--- src/core/features/features.hpp
+++ src/core/features/features.hpp
@@ -16,7 +16,7 @@ namespace visuals {
 
 	namespace glow {
 		void draw_glow();
-	};
+	}
 
 	namespace chams {
 		void draw_chams(i_mat_render_context* ctx, const draw_model_state_t& state, const model_render_info_t& info, matrix_t* matrix);
@@ -24,29 +24,29 @@ namespace visuals {
 
 	namespace misc {
 		void nade_predict() noexcept;
-	};
+	}
 
 	namespace entity_info {		// paint_traverse
 		void bomb(entity_t* bomb_ent);
 		void dropped_bomb(entity_t* bomb_ent);
 		void weapon_name(entity_t* entity, const char* text, int y_offset);
 		//void weapon_icon(entity_t* entity, int class_id);
-	};
+	}
 
 	namespace crosshair {
 		void draw_custom_crosshair(int x, int y, int outline, color cross_color);
 		void custom_crosshair();
 		void recoil_crosshair();
-	};
-};
+	}
+}
 
 namespace misc {
 	namespace movement {
 		void bunny_hop(c_usercmd* cmd);
-	};
+	}
 	void spectator_list();
-};
+}
 
 namespace commands_features {
 	void exec_autoexec();
-};
+}
