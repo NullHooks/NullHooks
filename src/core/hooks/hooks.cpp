17349dac1c62d17d47c7fd7220a4687214b9fd2e
Null <29655971+r4v10l1@users.noreply.github.com>
2022-06-20 13:01:07 +0200

    Format hooks.cpp
    Format to match the project's style and revert some style changes

--- src/core/hooks/hooks.cpp
+++ src/core/hooks/hooks.cpp
@@ -2,13 +2,12 @@
 #include "hooks.hpp"
 #include "../menu/menu.hpp"
 
-HWND						hooks::hCSGOWindow = nullptr;
-WNDPROC						hooks::pOriginalWNDProc = nullptr;
+HWND                        hooks::hCSGOWindow = nullptr;
+WNDPROC                     hooks::pOriginalWNDProc = nullptr;
 
 extern LRESULT ImGui_ImplDX9_WndProcHandler(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam);
 
-void InitImGui(LPDIRECT3DDEVICE9 pDevice)
-{
+void InitImGui(LPDIRECT3DDEVICE9 pDevice) {
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO();
 	menu.apply_fonts();
@@ -28,42 +27,40 @@ bool hooks::initialize() {
 	const auto reset = reinterpret_cast<void*>(get_virtual(interfaces::directx, 16));
 
 	// Get window handle
-	while (!(hooks::hCSGOWindow = FindWindowA("Valve001", nullptr)))
-	{
+	while (!(hooks::hCSGOWindow = FindWindowA("Valve001", nullptr))) {
 		using namespace std::literals::chrono_literals;
 		std::this_thread::sleep_for(50ms);
 	}
 
-	if (hooks::hCSGOWindow)        // Hook WNDProc to capture mouse / keyboard input
+	if (hooks::hCSGOWindow) {       // Hook WNDProc to capture mouse / keyboard input
 		hooks::pOriginalWNDProc = reinterpret_cast<WNDPROC>(SetWindowLongPtr(hooks::hCSGOWindow, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(hooks::WndProc)));
-	else
-	{
+    } else {
 		throw std::runtime_error("failed to hook wndproc.");
 		return false;
 	}
 
 	if (MH_Initialize() != MH_OK)
-		throw std::runtime_error("failed to initialize MH_Initialize.");
+		throw std::runtime_error("failed to initialize minhook.");
 
 	if (MH_CreateHook(alloc_key_values_target, &alloc_key_values_memory::hook, reinterpret_cast<void**>(&alloc_key_values_memory::original)) != MH_OK)
 		throw std::runtime_error("failed to initialize alloc_key_values_memory.");
-	custom_helpers::state_to_console("Hooks", "alloc_key_values_memory initialized!");
+	custom_helpers::state_to_console_color("Hooks", "alloc_key_values_memory initialized!");
 
 	if (MH_CreateHook(create_move_target, &create_move::hook, reinterpret_cast<void**>(&create_move::original)) != MH_OK)
 		throw std::runtime_error("failed to initialize create_move. (outdated index?)");
-	custom_helpers::state_to_console("Hooks", "create_move initialized!");
+	custom_helpers::state_to_console_color("Hooks", "create_move initialized!");
 
 	if (MH_CreateHook(paint_traverse_target, &paint_traverse::hook, reinterpret_cast<void**>(&paint_traverse::original)) != MH_OK)
 		throw std::runtime_error("failed to initialize paint_traverse. (outdated index?)");
-	custom_helpers::state_to_console("Hooks", "paint_traverse initialized!");
+	custom_helpers::state_to_console_color("Hooks", "paint_traverse initialized!");
 
 	if (MH_CreateHook(post_screen_space_effects_target, &do_post_screen_space_effects::hook, reinterpret_cast<void**>(&do_post_screen_space_effects::original)) != MH_OK)
 		throw std::runtime_error("failed to initialize do_post_screen_space_effects.");
-	custom_helpers::state_to_console("Hooks", "do_post_screen_space_effects initialized!");
+	custom_helpers::state_to_console_color("Hooks", "do_post_screen_space_effects initialized!");
 
 	if (MH_CreateHook(get_viewmodel_fov_target, &get_viewmodel_fov::hook, reinterpret_cast<void**>(&get_viewmodel_fov::original)) != MH_OK)
 		throw std::runtime_error("failed to initialize get_viewmodel_fov.");
-	custom_helpers::state_to_console("Hooks", "get_viewmodel_fov initialized!");
+	custom_helpers::state_to_console_color("Hooks", "get_viewmodel_fov initialized!");
 
 	if (MH_CreateHook(findmdl_target, &findmdl::hook, reinterpret_cast<void**>(&findmdl::original)) != MH_OK)
 		throw std::runtime_error("failed to initialize findmdl.");
@@ -71,22 +68,25 @@ bool hooks::initialize() {
 
 	if (MH_CreateHook(override_view_target, &override_view::hook, reinterpret_cast<void**>(&override_view::original)) != MH_OK)
 		throw std::runtime_error("failed to initialize override_view.");
-	custom_helpers::state_to_console("Hooks", "override_view initialized!");
+	custom_helpers::state_to_console_color("Hooks", "override_view initialized!");
 
 	if (MH_CreateHook(draw_model_execute_target, &draw_model_execute::hook, reinterpret_cast<void**>(&draw_model_execute::original)) != MH_OK)
 		throw std::runtime_error("failed to initialize draw_model_execute.");
-	custom_helpers::state_to_console("Hooks", "draw_model_execute initialized!");
+	custom_helpers::state_to_console_color("Hooks", "draw_model_execute initialized!");
 
 	if (MH_CreateHook(present, &present::hook, reinterpret_cast<void**>(&present::original)) != MH_OK)
 		throw std::runtime_error("failed to initialize present hook.");
-
+	custom_helpers::state_to_console_color("Hooks", "present hook initialized!");
+    
 	if (MH_CreateHook(reset, &reset::hook, reinterpret_cast<void**>(&reset::original)) != MH_OK)
 		throw std::runtime_error("failed to initialize reset hook.");
+	custom_helpers::state_to_console_color("Hooks", "reset hook initialized!");
 
 	if (MH_EnableHook(MH_ALL_HOOKS) != MH_OK)
 		throw std::runtime_error("failed to enable hooks.");
-
 	
+    custom_helpers::state_to_console_color("Hooks", "Hooks initialized!");
+    
 	/* ------------------------------------------------------------------------ */
 
 	// Reset crosshair
@@ -103,6 +103,8 @@ bool hooks::initialize() {
 }
 
 void hooks::release() {
+    custom_helpers::state_to_console_color("Unhook", "Unhooking the cheat...\n");
+    
 	menu.menu_opened = false;
 
 	MH_DisableHook(MH_ALL_HOOKS);
@@ -112,15 +114,14 @@ void hooks::release() {
 
 bool init = false;
 
-long __stdcall hooks::present::hook(IDirect3DDevice9* device, RECT* source_rect, RECT* dest_rect, HWND dest_window_override, RGNDATA* dirty_region)
-{
-	if (!init)
-	{
+long __stdcall hooks::present::hook(IDirect3DDevice9* device, RECT* source_rect, RECT* dest_rect, HWND dest_window_override, RGNDATA* dirty_region) {
+	if (!init) {
 		menu.apply_fonts();
 		menu.setup_resent(device);
 		init = true;
-	}
-	if (init) {
+    }
+	
+    if (init) {
 		menu.pre_render(device);
 		menu.post_render();
 
@@ -132,8 +133,7 @@ long __stdcall hooks::present::hook(IDirect3DDevice9* device, RECT* source_rect,
 	return present::original(device, source_rect, dest_rect, dest_window_override, dirty_region);
 }
 
-long __stdcall hooks::reset::hook(IDirect3DDevice9* device, D3DPRESENT_PARAMETERS* present_parameters)
-{
+long __stdcall hooks::reset::hook(IDirect3DDevice9* device, D3DPRESENT_PARAMETERS* present_parameters) {
 	if (!init)
 		reset::original(device, present_parameters);
 
@@ -144,8 +144,7 @@ long __stdcall hooks::reset::hook(IDirect3DDevice9* device, D3DPRESENT_PARAMETER
 	return hr;
 }
 
-LRESULT __stdcall hooks::WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
-{
+LRESULT __stdcall hooks::WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam) {
 	if (GetAsyncKeyState(VK_INSERT) & 1)
 		menu.menu_opened = !menu.menu_opened;
 
