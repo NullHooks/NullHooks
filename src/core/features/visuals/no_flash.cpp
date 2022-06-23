26decffd7bd963e83bf79c0fc886ad18fd248bb2
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-06-23 22:14:50 +0200

    Add alpha slider to noflash

--- src/core/features/visuals/no_flash.cpp
+++ src/core/features/visuals/no_flash.cpp
@@ -4,13 +4,11 @@ void visuals::noflash() {
 	if (!interfaces::engine->is_connected() && !interfaces::engine->is_in_game()) return;
 	if (!csgo::local_player) return;
 
-	if (!variables::misc_visuals::noflash) {
+	if (variables::misc_visuals::noflash_alpha >= 255.f) {
 		if (csgo::local_player->flash_alpha() < 255.f)
 			csgo::local_player->flash_alpha() = 255.f;
 		return;
 	}
 
-	// TODO: Add slider for intensity?
-	if (csgo::local_player->flash_alpha() > 0.f)
-		csgo::local_player->flash_alpha() = 0.f;
+	csgo::local_player->flash_alpha() = 255.f * variables::misc_visuals::noflash_alpha;
 }
