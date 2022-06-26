9e292f582e0b78cf3ace06cd68f06c08cdfc9b07
hBuffer <105053604+hBuffer@users.noreply.github.com>
2022-06-26 22:59:54 +0100

    update speedgraph in createmove

--- src/core/features/misc/speedgraph.cpp
+++ src/core/features/misc/speedgraph.cpp
@@ -15,40 +15,39 @@ color speed2color(int speed) {
 	return custom_helpers::hsv2color(hue, 255);
 }
 
-void misc::speed_graph() {
+void misc::speedgraph::update() {
+	const int cur_speed = (int)std::ceil(csgo::local_player->velocity().length_2d());
+	shift_and_append(cur_speed);
+}
+
+void misc::speedgraph::draw() {
 	if (!variables::misc::draw_speedgraph) return;
 	if (!interfaces::engine->is_in_game() || !interfaces::engine->is_connected()) return;
 	if (!csgo::local_player) return;
 	if (!csgo::local_player->is_alive()) return;
 	if (csgo::local_player->is_scoped()) return;		// We don't want to draw speed graph when scoped
 
-	const int cur_speed = (int)std::ceil(csgo::local_player->velocity().length_2d());
-
-	static int old_tick_count = interfaces::globals->tick_count;
-
-	if(interfaces::globals->tick_count != old_tick_count) {
-		shift_and_append(cur_speed);
-		old_tick_count = interfaces::globals->tick_count;
-	}
-
 	// Render graph
 	int screen_w, screen_h;
 	interfaces::surface->get_screen_size(screen_w, screen_h);
 
-	for (int n = 0; n < speeds_vec.size(); n++) {
-		int n_speed = speeds_vec.at(n);
+	for (int n = 0; n < speeds_vec.size() - 1; n++) { // -1 to skip last item
+		int cur_speed  = speeds_vec.at(n);
+		int next_speed = speeds_vec.at(n + 1);
 
-		int cur_x = screen_w / 2 - speed_graph_width / 2 + n;
-		int next_x = (n != speeds_vec.size() - 1) ? cur_x + 1 : cur_x;
-		int cur_y = screen_h * 0.9 - n_speed * 0.3;
-		int next_y = (n != speeds_vec.size() - 1) ? screen_h * 0.9 - speeds_vec.at(n + 1) * 0.3 : cur_y;
+		int cur_x  = screen_w / 2 - speed_graph_width / 2 + n;
+		int next_x = cur_x + 1;
+		int cur_y  = screen_h * 0.9 - cur_speed  * 0.3;
+		int next_y = screen_h * 0.9 - next_speed * 0.3;
 
 		color line_col = color::white();
 		if (variables::misc::use_speedgraph_color)
-			line_col = speed2color(n_speed);
+			line_col = speed2color(next_speed);
 
 		render::draw_line(cur_x, cur_y, next_x, next_y, line_col);
 	}
+
+	const int cur_speed = (int)std::ceil(csgo::local_player->velocity().length_2d());
 
 	// Speed text
 	color speed_col = speed2color(cur_speed);
