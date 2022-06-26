43269848f8c51d829bf3c8d10b619ea9dadebe88
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-06-26 17:53:55 +0200

    Add path checks to model changer

--- src/core/features/visuals/models.cpp
+++ src/core/features/visuals/models.cpp
@@ -10,40 +10,43 @@
  */
 
 void visuals::models::replace_model(char* path) {
+    std::string model_path = "csgo/";
     if (strstr(path, "v_")) {
         if (strstr(path, "knife_default_ct.mdl") || strstr(path, "knife_default_t.mdl")) {          // What model are wen changing
-            if (variables::models::v_knife_path != NULL) {
+            //printf("%s", model_path.append(variables::models::v_knife_path));
+            //printf("%d", std::filesystem::exists(model_path.append(variables::models::v_knife_path)));
+            if (variables::models::v_knife_path != NULL && std::filesystem::exists(model_path.append(variables::models::v_knife_path))) {
                 sprintf(path, variables::models::v_knife_path);                                     // The model we want to replace it with
                 custom_helpers::state_to_console_color("Models", "Replaced knife viewmodel");
             }
         } else if (strstr(path, "v_rif_ak47.mdl")) {
-            if (variables::models::v_ak_path != NULL) {
+            if (variables::models::v_ak_path != NULL && std::filesystem::exists(model_path.append(variables::models::v_ak_path))) {
                 sprintf(path, variables::models::v_ak_path);
                 custom_helpers::state_to_console_color("Models", "Replaced ak viewmodel");
             }
         }
     } else if (strstr(path, "w_")) {
         if (strstr(path, "knife_default_ct.mdl") || strstr(path, "knife_default_t.mdl")) {
-            if (variables::models::w_knife_path != NULL) {
+            if (variables::models::w_knife_path != NULL && std::filesystem::exists(model_path.append(variables::models::w_knife_path))) {
                 sprintf(path, variables::models::w_knife_path);
                 custom_helpers::state_to_console_color("Models", "Replaced knife worldmodel");
             }
         } else if (strstr(path, "v_rif_ak47.mdl")) {
-            if (variables::models::w_ak_path != NULL) {
+            if (variables::models::w_ak_path != NULL && std::filesystem::exists(model_path.append(variables::models::w_ak_path))) {
                 sprintf(path, variables::models::w_ak_path);
                 custom_helpers::state_to_console_color("Models", "Replaced ak worldmodel");
             }
         }
     } else {
         if (strstr(path, "models/player")) {
-            if (variables::models::player_path != NULL) {
+            if (variables::models::player_path != NULL && std::filesystem::exists(model_path.append(variables::models::player_path))) {
                 sprintf(path, variables::models::player_path);
                 custom_helpers::state_to_console_color("Models", "Replaced player model");
             }
         }
         
         if (strstr(path, "t_arms_")) {
-            if (variables::models::arms_path != NULL) {
+            if (variables::models::arms_path != NULL && std::filesystem::exists(model_path.append(variables::models::arms_path))) {
                 sprintf(path, variables::models::arms_path);
                 custom_helpers::state_to_console_color("Models", "Replaced arms model");
             }
