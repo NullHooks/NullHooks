9ba4a10daa559850af4ba2f1b29556dac580438e
hBuffer <105053604+hBuffer@users.noreply.github.com>
2022-06-28 00:01:44 +0100

    clean up vcxproj file

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,5 +1,6 @@
 #include "core/features/features.hpp"
 #include "core/menu/variables.hpp"
+#include "dependencies/utilities/renderer/renderer.hpp"
 
 unsigned long WINAPI initialize(void* instance) {
 	while (!GetModuleHandleA("serverbrowser.dll"))
