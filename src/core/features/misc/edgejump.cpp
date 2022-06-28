5f6a85fa391939f4a972ca6d51f411ba8e66c687
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-06-29 00:48:17 +0200

    Remove comment from edgejump.cpp

--- src/core/features/misc/edgejump.cpp
+++ src/core/features/misc/edgejump.cpp
@@ -4,7 +4,7 @@
 
 void misc::movement::edgejump(c_usercmd* cmd, int old_flags) {
 	if (!variables::misc::edgejump) return;
-	if (!input::gobal_input.IsHeld(variables::misc::ej_key)) return;	// Key (PAGE UP until I add key thing to framework)
+	if (!input::gobal_input.IsHeld(variables::misc::ej_key)) return;
 	if (!interfaces::engine->is_connected() || !interfaces::engine->is_in_game()) return;
 	if (!csgo::local_player) return;
 	if (csgo::local_player->move_type() == movetype_ladder || csgo::local_player->move_type() == movetype_noclip) return;
