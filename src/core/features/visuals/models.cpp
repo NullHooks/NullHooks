be8a73ad7530d7f47a299c5e850d56d2c8c79305
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-06-28 18:03:03 +0200

    Made model changer a map

--- src/core/features/visuals/models.cpp
+++ src/core/features/visuals/models.cpp
@@ -3,78 +3,95 @@
 #include "core/features/visuals/models.hpp"
 
 void visuals::custom_models::replace_model(char* path) {
+    const char* current_model = NULL;   // Will change depending on the path (func arg) in each conditional
     std::string model_path = "csgo/";
     
     // Viewmodels
     if (strstr(path, "v_")) {
         if (strstr(path, "knife_default_ct.mdl") || strstr(path, "knife_default_t.mdl")) {          // What model are wen changing
-            //printf("%s", model_path.append(models::v_knife_path));
-            //printf("%d", std::filesystem::exists(model_path.append(models::v_knife_path)));
-            if (models::v_knife_path != NULL && std::filesystem::exists(model_path.append(models::v_knife_path))) {
-                sprintf(path, models::v_knife_path);                                     // The model we want to replace it with
+            current_model = models::weapon_models[WEAPON_KNIFE].viewmodel;
+            if (current_model != NULL && std::filesystem::exists(model_path.append(current_model))) {
+                sprintf(path, current_model);                                     // The model we want to replace it with
                 custom_helpers::state_to_console_color("Models", "Replaced knife viewmodel");
             }
         } else if (strstr(path, "v_rif_ak47.mdl")) {
-            if (models::v_ak_path != NULL && std::filesystem::exists(model_path.append(models::v_ak_path))) {
-                sprintf(path, models::v_ak_path);
-                custom_helpers::state_to_console_color("Models", "Replaced ak viewmodel");
+            current_model = models::weapon_models[WEAPON_AK47].viewmodel;
+            if (current_model != NULL && std::filesystem::exists(model_path.append(current_model))) {
+                sprintf(path, current_model);
+                custom_helpers::state_to_console_color("Models", "Replaced ak47 viewmodel");
             }
         } else if (strstr(path, "v_snip_awp.mdl")) {
-            if (models::v_awp_path != NULL && std::filesystem::exists(model_path.append(models::v_awp_path))) {
-                sprintf(path, models::v_awp_path);
+            current_model = models::weapon_models[WEAPON_AWP].viewmodel;
+            if (current_model != NULL && std::filesystem::exists(model_path.append(current_model))) {
+                sprintf(path, current_model);
                 custom_helpers::state_to_console_color("Models", "Replaced awp viewmodel");
             }
         } else if (strstr(path, "v_snip_ssg08.mdl")) {
-            if (models::v_ssg08_path != NULL && std::filesystem::exists(model_path.append(models::v_ssg08_path))) {
-                sprintf(path, models::v_ssg08_path);
+            current_model = models::weapon_models[WEAPON_SSG08].viewmodel;
+            if (current_model != NULL && std::filesystem::exists(model_path.append(current_model))) {
+                sprintf(path, current_model);
                 custom_helpers::state_to_console_color("Models", "Replaced ssg08 viewmodel");
             }
         }
     // Worldmodels
     } else if (strstr(path, "w_")) {
         if (strstr(path, "knife_default_ct.mdl") || strstr(path, "knife_default_t.mdl")) {
-            if (models::w_knife_path != NULL && std::filesystem::exists(model_path.append(models::w_knife_path))) {
-                sprintf(path, models::w_knife_path);
+            current_model = models::weapon_models[WEAPON_KNIFE].worldmodel;
+            if (current_model != NULL && std::filesystem::exists(model_path.append(current_model))) {
+                sprintf(path, current_model);
                 custom_helpers::state_to_console_color("Models", "Replaced knife worldmodel");
             }
         } else if (strstr(path, "w_rif_ak47.mdl")) {
-            if (models::w_ak_path != NULL && std::filesystem::exists(model_path.append(models::w_ak_path))) {
-                sprintf(path, models::w_ak_path);
-                custom_helpers::state_to_console_color("Models", "Replaced ak worldmodel");
+            current_model = models::weapon_models[WEAPON_AK47].worldmodel;
+            if (current_model != NULL && std::filesystem::exists(model_path.append(current_model))) {
+                sprintf(path, current_model);
+                custom_helpers::state_to_console_color("Models", "Replaced ak47 worldmodel");
+            }
+        } else if (strstr(path, "w_rif_ak47_dropped.mdl")) {
+            current_model = models::weapon_models[WEAPON_AK47].dropped_worldmodel;
+            if (current_model != NULL && std::filesystem::exists(model_path.append(current_model))) {
+                sprintf(path, current_model);
+                custom_helpers::state_to_console_color("Models", "Replaced dropped ak47 worldmodel");
             }
         } else if (strstr(path, "w_snip_awp.mdl")) {
-            if (models::w_awp_path != NULL && std::filesystem::exists(model_path.append(models::w_awp_path))) {
-                sprintf(path, models::w_awp_path);
+            current_model = models::weapon_models[WEAPON_AWP].worldmodel;
+            if (current_model != NULL && std::filesystem::exists(model_path.append(current_model))) {
+                sprintf(path, current_model);
                 custom_helpers::state_to_console_color("Models", "Replaced awp worldmodel");
             }
         } else if (strstr(path, "w_snip_awp_dropped.mdl")) {
-            if (models::w_awp_drop_path != NULL && std::filesystem::exists(model_path.append(models::w_awp_drop_path))) {
-                sprintf(path, models::w_awp_drop_path);
+            current_model = models::weapon_models[WEAPON_AWP].dropped_worldmodel;
+            if (current_model != NULL && std::filesystem::exists(model_path.append(current_model))) {
+                sprintf(path, current_model);
                 custom_helpers::state_to_console_color("Models", "Replaced dropped awp worldmodel");
             }
         } else if (strstr(path, "w_snip_ssg08.mdl")) {
-            if (models::w_ssg08_path != NULL && std::filesystem::exists(model_path.append(models::w_ssg08_path))) {
-                sprintf(path, models::w_ssg08_path);
+            current_model = models::weapon_models[WEAPON_SSG08].worldmodel;
+            if (current_model != NULL && std::filesystem::exists(model_path.append(current_model))) {
+                sprintf(path, current_model);
                 custom_helpers::state_to_console_color("Models", "Replaced ssg08 worldmodel");
             }
         } else if (strstr(path, "w_snip_ssg08_dropped.mdl")) {
-            if (models::w_ssg08_drop_path != NULL && std::filesystem::exists(model_path.append(models::w_ssg08_drop_path))) {
-                sprintf(path, models::w_ssg08_drop_path);
+            current_model = models::weapon_models[WEAPON_SSG08].dropped_worldmodel;
+            if (current_model != NULL && std::filesystem::exists(model_path.append(current_model))) {
+                sprintf(path, current_model);
                 custom_helpers::state_to_console_color("Models", "Replaced dropped ssg08 worldmodel");
             }
         }
     // Other (players, arms, etc.)
     } else {
         if (strstr(path, "models/player")) {
-            if (models::player_path != NULL && std::filesystem::exists(model_path.append(models::player_path))) {
-                sprintf(path, models::player_path);
+            current_model = models::misc_models[PLAYER_MODEL_ID];
+            if (current_model != NULL && std::filesystem::exists(model_path.append(current_model))) {
+                sprintf(path, current_model);
                 custom_helpers::state_to_console_color("Models", "Replaced player model");
             }
         }
         
         if (strstr(path, "t_arms")) {
-            if (models::arms_path != NULL && std::filesystem::exists(model_path.append(models::arms_path))) {
-                sprintf(path, models::arms_path);
+            current_model = models::misc_models[ARMS_MODEL_ID];
+            if (current_model != NULL && std::filesystem::exists(model_path.append(current_model))) {
+                sprintf(path, current_model);
                 custom_helpers::state_to_console_color("Models", "Replaced arms model");
             }
         }
--- src/core/features/visuals/models.hpp
+++ src/core/features/visuals/models.hpp
@@ -15,17 +15,121 @@
  *     w_*_dropped.mdl -> dropped worldmodel path
  */
 
+enum {
+	ARMS_MODEL_ID = 0,
+	PLAYER_MODEL_ID
+};
+
+struct custom_model {
+	const char* viewmodel;				// The viewmodel path			(v_*.mdl        )
+	const char* worldmodel;				// The worldmodel path			(w_*.mdl        )
+	const char* dropped_worldmodel;		// The dropped worldmodel path	(w_*_dropped.mdl)
+};
+
 namespace models {
-	inline const char* v_knife_path = NULL;			// "models/weapons/v_minecraft_pickaxe.mdl" | "models/weapons/caleon1/screwdriver/v_knife_screwdriver.mdl" | "models/weapons/eminem/bananabit/v_bananabit.mdl"
-	inline const char* w_knife_path = NULL;			// "models/weapons/w_minecraft_pickaxe.mdl"
-	inline const char* v_ak_path = "models/weapons/eminem/ethereal/v_ethereal.mdl";			// "models/weapons/eminem/ethereal/v_ethereal.mdl"
-	inline const char* w_ak_path = NULL;			// "models/weapons/eminem/ethereal/w_ethereal.mdl"
-	inline const char* v_awp_path = NULL;			// "models/weapons/eminem/dsr_50/v_dsr_50_2.mdl"
-	inline const char* w_awp_path = NULL;			// "models/weapons/eminem/dsr_50/w_dsr_50_2.mdl"
-	inline const char* w_awp_drop_path = NULL;		// "models/weapons/eminem/dsr_50/w_dsr_50_dropped.mdl"
-	inline const char* v_ssg08_path = NULL;			// "models/weapons/eminem/crysis3_predator_bow/v_crysis3_predator_bow.mdl"
-	inline const char* w_ssg08_path = NULL;			// "models/weapons/eminem/crysis3_predator_bow/w_crysis3_predator_bow.mdl"
-	inline const char* w_ssg08_drop_path = NULL;	// "models/weapons/eminem/crysis3_predator_bow/w_crysis3_predator_bow_dropped.mdl"
-	inline const char* player_path = NULL;			// Kinda broken
-	inline const char* arms_path = NULL;
+	std::map<int, custom_model> weapon_models = {
+	/*   Weapon id                           Viewmodel, Worldmodel, Dropped worldmodel */
+		{ WEAPON_NONE,						{ NULL, NULL, NULL } },
+		{ WEAPON_DEAGLE,					{ NULL, NULL, NULL } },
+		{ WEAPON_ELITE,						{ NULL, NULL, NULL } },
+		{ WEAPON_FIVESEVEN,					{ NULL, NULL, NULL } },
+		{ WEAPON_GLOCK,						{ NULL, NULL, NULL } },
+		{ WEAPON_AK47,						{ "models/weapons/eminem/ethereal/v_ethereal.mdl", "models/weapons/eminem/ethereal/w_ethereal.mdl", "models/weapons/eminem/ethereal/w_ethereal_dropped.mdl" } },
+		{ WEAPON_AUG,						{ NULL, NULL, NULL } },
+		{ WEAPON_AWP,						{ "models/weapons/eminem/dsr_50/v_dsr_50_2.mdl", "models/weapons/eminem/dsr_50/w_dsr_50_2.mdl", "models/weapons/eminem/dsr_50/w_dsr_50_dropped.mdl" } },
+		{ WEAPON_FAMAS,						{ NULL, NULL, NULL } },
+		{ WEAPON_G3SG1,						{ NULL, NULL, NULL } },
+		{ WEAPON_GALILAR,					{ NULL, NULL, NULL } },
+		{ WEAPON_M249,						{ NULL, NULL, NULL } },
+		{ WEAPON_M4A1,						{ NULL, NULL, NULL } },
+		{ WEAPON_MAC10,						{ NULL, NULL, NULL } },
+		{ WEAPON_P90,						{ NULL, NULL, NULL } },
+		{ WEAPON_MP5SD,						{ NULL, NULL, NULL } },
+		{ WEAPON_UMP45,						{ NULL, NULL, NULL } },
+		{ WEAPON_XM1014,					{ NULL, NULL, NULL } },
+		{ WEAPON_BIZON,						{ NULL, NULL, NULL } },
+		{ WEAPON_MAG7,						{ NULL, NULL, NULL } },
+		{ WEAPON_NEGEV,						{ NULL, NULL, NULL } },
+		{ WEAPON_SAWEDOFF,					{ NULL, NULL, NULL } },
+		{ WEAPON_TEC9,						{ NULL, NULL, NULL } },
+		{ WEAPON_TASER,						{ NULL, NULL, NULL } },
+		{ WEAPON_HKP2000,					{ NULL, NULL, NULL } },
+		{ WEAPON_MP7,						{ NULL, NULL, NULL } },
+		{ WEAPON_MP9,						{ NULL, NULL, NULL } },
+		{ WEAPON_NOVA,						{ NULL, NULL, NULL } },
+		{ WEAPON_P250,						{ NULL, NULL, NULL } },
+		{ WEAPON_SHIELD,					{ NULL, NULL, NULL } },
+		{ WEAPON_SCAR20,					{ NULL, NULL, NULL } },
+		{ WEAPON_SG556,						{ NULL, NULL, NULL } },
+		{ WEAPON_SSG08,						{ NULL, NULL, NULL } },
+		{ WEAPON_KNIFEGG,					{ NULL, NULL, NULL } },
+		{ WEAPON_KNIFE,						{ "models/weapons/caleon1/screwdriver/v_knife_screwdriver.mdl", NULL, NULL } },
+		{ WEAPON_FLASHBANG,					{ NULL, NULL, NULL } },
+		{ WEAPON_HEGRENADE,					{ NULL, NULL, NULL } },
+		{ WEAPON_SMOKEGRENADE,				{ NULL, NULL, NULL } },
+		{ WEAPON_MOLOTOV,					{ NULL, NULL, NULL } },
+		{ WEAPON_DECOY,						{ NULL, NULL, NULL } },
+		{ WEAPON_INCGRENADE,				{ NULL, NULL, NULL } },
+		{ WEAPON_C4,						{ NULL, NULL, NULL } },
+		{ WEAPON_HEALTHSHOT,				{ NULL, NULL, NULL } },
+		{ WEAPON_KNIFE_T,					{ NULL, NULL, NULL } },
+		{ WEAPON_M4A1_SILENCER,				{ NULL, NULL, NULL } },
+		{ WEAPON_USP_SILENCER,				{ NULL, NULL, NULL } },
+		{ WEAPON_CZ75A,						{ NULL, NULL, NULL } },
+		{ WEAPON_REVOLVER,					{ NULL, NULL, NULL } },
+		{ WEAPON_TAGRENADE,					{ NULL, NULL, NULL } },
+		{ WEAPON_FISTS,						{ NULL, NULL, NULL } },
+		{ WEAPON_BREACHCHARGE,				{ NULL, NULL, NULL } },
+		{ WEAPON_TABLET,					{ NULL, NULL, NULL } },
+		{ WEAPON_MELEE,						{ NULL, NULL, NULL } },
+		{ WEAPON_AXE,						{ NULL, NULL, NULL } },
+		{ WEAPON_HAMMER,					{ NULL, NULL, NULL } },
+		{ WEAPON_SPANNER,					{ NULL, NULL, NULL } },
+		{ WEAPON_KNIFE_GHOST,				{ NULL, NULL, NULL } },
+		{ WEAPON_FIREBOMB,					{ NULL, NULL, NULL } },
+		{ WEAPON_DIVERSION,					{ NULL, NULL, NULL } },
+		{ WEAPON_FRAG_GRENADE,				{ NULL, NULL, NULL } },
+		{ WEAPON_SNOWBALL,					{ NULL, NULL, NULL } },
+		{ WEAPON_BUMPMINE,					{ NULL, NULL, NULL } },
+		{ WEAPON_BAYONET,					{ NULL, NULL, NULL } },
+		{ WEAPON_KNIFE_CSS,					{ NULL, NULL, NULL } },
+		{ WEAPON_KNIFE_FLIP,				{ NULL, NULL, NULL } },
+		{ WEAPON_KNIFE_GUT,					{ NULL, NULL, NULL } },
+		{ WEAPON_KNIFE_KARAMBIT,			{ NULL, NULL, NULL } },
+		{ WEAPON_KNIFE_M9_BAYONET,			{ NULL, NULL, NULL } },
+		{ WEAPON_KNIFE_TACTICAL,			{ NULL, NULL, NULL } },
+		{ WEAPON_KNIFE_FALCHION,			{ NULL, NULL, NULL } },
+		{ WEAPON_KNIFE_SURVIVAL_BOWIE,		{ NULL, NULL, NULL } },
+		{ WEAPON_KNIFE_BUTTERFLY,			{ NULL, NULL, NULL } },
+		{ WEAPON_KNIFE_PUSH,				{ NULL, NULL, NULL } },
+		{ WEAPON_KNIFE_URSUS,				{ NULL, NULL, NULL } },
+		{ WEAPON_KNIFE_GYPSY_JACKKNIFE,		{ NULL, NULL, NULL } },
+		{ WEAPON_KNIFE_STILETTO,			{ NULL, NULL, NULL } },
+		{ WEAPON_KNIFE_WIDOWMAKER,			{ NULL, NULL, NULL } },
+		{ GLOVE_STUDDED_BLOODHOUND,			{ NULL, NULL, NULL } },
+		{ GLOVE_T_SIDE,						{ NULL, NULL, NULL } },
+		{ GLOVE_CT_SIDE,					{ NULL, NULL, NULL } },
+		{ GLOVE_SPORTY,						{ NULL, NULL, NULL } },
+		{ GLOVE_SLICK,						{ NULL, NULL, NULL } },
+		{ GLOVE_LEATHER_WRAP,				{ NULL, NULL, NULL } },
+		{ GLOVE_MOTORCYCLE,					{ NULL, NULL, NULL } },
+		{ GLOVE_SPECIALIST,					{ NULL, NULL, NULL } },
+		{ GLOVE_HYDRA,						{ NULL, NULL, NULL } }
+	};
+
+	std::map<int, const char*> misc_models = {
+	/*   Model id                           Model path */
+		{ ARMS_MODEL_ID,					NULL },
+		{ PLAYER_MODEL_ID,					NULL }	// Kinda broken
+	};
+
+	/*
+	 * Some interesting models:
+	 *     "models/weapons/v_minecraft_pickaxe.mdl"
+	 *	   "models/weapons/caleon1/screwdriver/v_knife_screwdriver.mdl"
+	 *     "models/weapons/eminem/bananabit/v_bananabit.mdl"
+	 *     "models/weapons/eminem/ethereal/v_ethereal.mdl"
+	 *     { "models/weapons/eminem/dsr_50/v_dsr_50_2.mdl", "models/weapons/eminem/dsr_50/w_dsr_50_2.mdl", "models/weapons/eminem/dsr_50/w_dsr_50_dropped.mdl" }
+	 *     "models/weapons/eminem/crysis3_predator_bow/v_crysis3_predator_bow.mdl"
+	 */
 }
