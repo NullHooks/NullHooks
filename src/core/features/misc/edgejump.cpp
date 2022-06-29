94950bfd563f267aa252b9bf8ad8d2eee7964c25
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-06-29 20:55:34 +0200

    Add alive check to jumpbug and edgejump

--- src/core/features/misc/edgejump.cpp
+++ src/core/features/misc/edgejump.cpp
@@ -7,6 +7,7 @@ void misc::movement::edgejump(c_usercmd* cmd, int old_flags) {
 	if (!input::gobal_input.IsHeld(variables::misc::ej_key)) return;
 	if (!interfaces::engine->is_connected() || !interfaces::engine->is_in_game()) return;
 	if (!csgo::local_player) return;
+	if (!csgo::local_player->is_alive()) return;
 	if (csgo::local_player->move_type() == movetype_ladder || csgo::local_player->move_type() == movetype_noclip) return;
 
 	if (old_flags & fl_onground && !(csgo::local_player->flags() & fl_onground))
