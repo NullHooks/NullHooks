1323524771e22671542ff7032ff5a1066bc19f2a
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-06-29 02:17:52 +0200

    Replace jumpbug

--- src/core/features/misc/jumpbug.cpp
+++ src/core/features/misc/jumpbug.cpp
@@ -2,45 +2,29 @@
 #include "core/features/features.hpp"
 #include "core/menu/variables.hpp"
 
-bool should_duck = false;
-int old_flags = 0;
-float height_diff;
-
-void misc::movement::pre_pred_jumpbug(c_usercmd* cmd) {
+void misc::movement::pre_pred_jumpbug(c_usercmd* cmd, int old_flags) {
     if (false) return;
+    // check key
     if (!interfaces::engine->is_connected() || !interfaces::engine->is_in_game()) return;
     if (!csgo::local_player) return;
     if (csgo::local_player->move_type() == movetype_ladder || csgo::local_player->move_type() == movetype_noclip) return;
 
-    old_flags = csgo::local_player->flags();
-    if (should_duck && !(old_flags & fl_onground))
-        cmd->buttons |= in_duck;
+    cmd->buttons |= in_bullrush;    // Infinite duck
 
-    vec3_t origin = csgo::local_player->origin();
-
-    ray_t ray;
-    ray.initialize(origin, vec3_t(origin.x, origin.y, origin.z - 8192.0f));
-
-    trace_filter filter;
-    filter.skip = csgo::local_player;
-
-    trace_t trace;
-    interfaces::trace_ray->trace_ray(ray, MASK_SHOT, &filter, &trace);
-
-    height_diff = origin.z - trace.end.z;
-    if (height_diff > 200.0f)
-        should_duck = true;
+    if ( variables::misc::bhop && !(old_flags & fl_onground) && cmd->buttons & in_jump)
+        cmd->buttons &= ~in_jump;   // Stop bhop
 }
 
-void misc::movement::post_pred_jumpbug(c_usercmd* cmd) {
+void misc::movement::post_pred_jumpbug(c_usercmd* cmd, int old_flags) {
     if (false) return;
+    // check key
     if (!interfaces::engine->is_connected() || !interfaces::engine->is_in_game()) return;
     if (!csgo::local_player) return;
     if (csgo::local_player->move_type() == movetype_ladder || csgo::local_player->move_type() == movetype_noclip) return;
 
-    if (!(old_flags & fl_onground) && (csgo::local_player->flags() & fl_onground) && should_duck) {
-        cmd->buttons &= ~in_duck;
-        //cmd->buttons &= in_jump;
-        should_duck = false;
-    }
+    if (!(old_flags & fl_onground) && (csgo::local_player->flags() & fl_onground))
+        cmd->buttons |= in_duck;
+
+    if (csgo::local_player->flags() & fl_onground)
+        cmd->buttons &= ~in_jump;
 }
