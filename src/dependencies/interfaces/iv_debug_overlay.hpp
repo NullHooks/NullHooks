24d65516e2ec8f025af541d69befa59308f9a4c2
hBuffer <105053604+hBuffer@users.noreply.github.com>
2022-07-02 10:44:50 +0100

    fix flickering

--- src/dependencies/interfaces/iv_debug_overlay.hpp
+++ src/dependencies/interfaces/iv_debug_overlay.hpp
@@ -1,16 +1,17 @@
 #pragma once
 #include "source-sdk/math/vector3d.hpp"
+#include "source-sdk/misc/color.hpp"
 
 class iv_debug_overlay {
 public:
-	void add_box_overlay(const vec3_t& vecOrigin, const vec3_t& vecAbsMin, const vec3_t& vecAbsMax, const vec3_t& angOrientation, int r, int g, int b, int a, float flDuration) {
+	void add_box_overlay(const vec3_t& vecOrigin, const vec3_t& vecAbsMin, const vec3_t& vecAbsMax, const vec3_t& angOrientation, color col, float flDuration) {
 		using original_fn = void(__thiscall*)(void*, const vec3_t&, const vec3_t&, const vec3_t&, const vec3_t&, int, int, int, int, float);
-		return (*(original_fn**)this)[1](this, vecOrigin, vecAbsMin, vecAbsMax, angOrientation, r, g, b, a, flDuration);
+		return (*(original_fn**)this)[1](this, vecOrigin, vecAbsMin, vecAbsMax, angOrientation, col.r, col.g, col.b, col.a, flDuration);
 	}
 
-	void add_line_overlay(const vec3_t& origin, const vec3_t& dest, int r, int g, int b, bool noDepthTest, float duration) {
+	void add_line_overlay(const vec3_t& origin, const vec3_t& dest, color col, bool noDepthTest, float duration) {
 		using original_fn = void(__thiscall*)(void*, const vec3_t&, const vec3_t&, int, int, int, bool, float);
-		return (*(original_fn**)this)[4](this, origin, dest, r, g, b, noDepthTest, duration);
+		return (*(original_fn**)this)[5](this, origin, dest, col.r, col.g, col.b, noDepthTest, duration);
 	}
 
 	bool world_to_screen(const vec3_t& in, vec3_t& out) {
