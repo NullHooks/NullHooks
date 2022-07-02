1133796a3adc58c8726df9c4a2ad6a5b853fa947
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-07-02 02:11:45 +0200

    Kinda fix c4 timer problem

--- src/core/hooks/functions/fire_event.cpp
+++ src/core/hooks/functions/fire_event.cpp
@@ -1,15 +1,21 @@
 #include "dependencies/utilities/csgo.hpp"
 #include "core/features/features.hpp"
 #include "core/hooks/hooks.hpp"
+#include "core/hooks/functions/event_globals.hpp"
 
-// SIG: "55 8B EC 83 E4 F8 83 EC 0C 53 8B D9 56 57 89 5C 24 0C" in engine
 void __fastcall hooks::fire_event::hook(void* thisptr, void* edx, i_game_event* gameEvent, bool bServerOnly, bool bClientOnly) {
     if (gameEvent) { // Nullptr check just incase!
         auto name = gameEvent->get_name();
 
-        // Add all your strcmp checks and function calls here
-        if (!strcmp(name, "player_hurt")) {
-            printf("Player hurt.\n");
+        // Event list: https://wiki.alliedmods.net/Counter-Strike:_Global_Offensive_Events
+        if (!strcmp(name, "bullet_impact")) {
+            printf("bullet_impact\n");
+        } else if (!strcmp(name, "round_end")) {
+            printf("round_end\n");
+            event_globals::round_ended = true;
+        } else if (!strcmp(name, "round_start")) {
+            printf("round_start\n");
+            event_globals::round_ended = false;
         }
     }
 
