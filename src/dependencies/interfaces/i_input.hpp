fbbb6c547d941cf83da6cd4517a5a8e81b0803dd
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-07-04 03:10:42 +0200

    Update input interface

--- src/dependencies/interfaces/i_input.hpp
+++ src/dependencies/interfaces/i_input.hpp
@@ -2,18 +2,21 @@
 #include "source-sdk/classes/c_usercmd.hpp"
 
 class i_input {
-private:
-	char u0[ 172 ];
-	bool u1;
 public:
-	bool camera_in_third_person;
-private:
-	char u2[ 2 ];
-public:
-	vec3_t camera_offset;
+	std::byte			pad0[0xC];				// 0x00
+	bool				bTrackIRAvailable;		// 0x0C
+	bool				bMouseInitialized;		// 0x0D
+	bool				bMouseActive;			// 0x0E
+	std::byte			pad1[0x9A];				// 0x0F
+	bool				camera_in_third_person;	// 0xA9
+	std::byte			pad2[0x2];				// 0xAA
+	vec3_t				camera_offset;			// 0xAC
+	std::byte			pad3[0x38];				// 0xB8
+	c_usercmd*          pCommands;				// 0xF0
+
 
 	c_usercmd *get_user_cmd( int slot, int sequence_num ) {
-		using fn = c_usercmd * ( __thiscall * )( void *, int, int );
-		return ( *( fn ** ) this )[ 8 ]( this, slot, sequence_num );
+		using fn = c_usercmd* (__thiscall*)(void *, int, int);
+		return (*( fn**)this)[8](this, slot, sequence_num);
 	}
 };
