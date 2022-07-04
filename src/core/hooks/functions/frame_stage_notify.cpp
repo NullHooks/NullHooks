60ce4737ceb574a6ce76a719488997cdb154e894
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-07-04 16:34:57 +0200

    Clean frame_stage_notify hook (temporary)

--- src/core/hooks/functions/frame_stage_notify.cpp
+++ src/core/hooks/functions/frame_stage_notify.cpp
@@ -3,14 +3,9 @@
 #include "core/menu/variables.hpp"
 #include "core/hooks/hooks.hpp"
 
-void __stdcall hooks::frame_stage_notify::hook(client_frame_stage_t frame_stage) {
-	static auto set_interpolation_flags = [](player_t* e, int flag) {
-		const auto var_map = (uintptr_t)e + 36;
-		const auto sz_var_map = *(int*)(var_map + 20);
-		for (auto index = 0; index < sz_var_map; index++)
-			*(uintptr_t*)((*(uintptr_t*)var_map) + index * 12) = flag;
-	};
+void run_backtrack();
 
+void __stdcall hooks::frame_stage_notify::hook(client_frame_stage_t frame_stage) {
 	switch (frame_stage) {
 		case FRAME_UNDEFINED:                       break;
 		case FRAME_START:                           break;
@@ -18,18 +13,30 @@ void __stdcall hooks::frame_stage_notify::hook(client_frame_stage_t frame_stage)
 		case FRAME_NET_UPDATE_POSTDATAUPDATE_START: break;
 		case FRAME_NET_UPDATE_POSTDATAUPDATE_END:   break;
 		case FRAME_NET_UPDATE_END:
-			if (true /*REPLACE VAR*/ && csgo::local_player && csgo::local_player->is_alive()) {
-				for (uint32_t i = 1; i <= interfaces::globals->max_clients; i++) {
-					player_t* player = reinterpret_cast<player_t*>(interfaces::entity_list->get_client_entity(i));
-					if (!player || player->team() == csgo::local_player->team() || player == csgo::local_player || player->dormant())
-						continue;
-					set_interpolation_flags(player, 0);
-				}
-			}
+			run_backtrack();
 			break;
 		case FRAME_RENDER_START:                    break;
 		case FRAME_RENDER_END:                      break;
 		default:                                    break;
 	}
+
 	original(interfaces::client, frame_stage);
+}
+
+// This is temporal
+void run_backtrack() {
+	static auto set_interpolation_flags = [](player_t* e, int flag) {
+		const auto var_map = (uintptr_t)e + 36;
+		const auto sz_var_map = *(int*)(var_map + 20);
+		for (auto index = 0; index < sz_var_map; index++)
+			*(uintptr_t*)((*(uintptr_t*)var_map) + index * 12) = flag;
+	};
+
+	if (!(true /*REPLACE VAR*/ && csgo::local_player && csgo::local_player->is_alive())) return;
+	for (uint32_t i = 1; i <= interfaces::globals->max_clients; i++) {
+		player_t* player = reinterpret_cast<player_t*>(interfaces::entity_list->get_client_entity(i));
+		if (!player || player->team() == csgo::local_player->team() || player == csgo::local_player || player->dormant())
+			continue;
+		set_interpolation_flags(player, 0);
+	}
 }
