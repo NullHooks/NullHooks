17756f9695e87fe47394d82343fb4f663b1cee7a
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-07-05 22:27:29 +0200

    Add comments to render_smoke_overlay.cpp

--- src/core/hooks/functions/render_smoke_overlay.cpp
+++ src/core/hooks/functions/render_smoke_overlay.cpp
@@ -4,13 +4,14 @@
 #include "core/menu/variables.hpp"
 
 // https://www.unknowncheats.me/forum/counterstrike-global-offensive/257450-removing-overlay-smoke.html#9
+// https://github.com/rollraw/qo0-base/blob/cb140182ee647dc3fd9ebcf156f29b1d7457ad95/base/core/hooks.cpp#L526-L535
 
 #define flSmokeIntensity_offset 0x588
 
 void __fastcall hooks::render_smoke_overlay::hook(void* thisptr, void* edx, bool previewmodel) {
 	// Only call original if we want to render smoke
 	if (variables::misc_visuals::wireframe_smoke)
-		*reinterpret_cast<float*>(std::uintptr_t(interfaces::view_render) + flSmokeIntensity_offset) = 0.0f;
+		*reinterpret_cast<float*>(std::uintptr_t(interfaces::view_render) + flSmokeIntensity_offset) = 0.0f;	// Used to remove small overlay from hands, etc.
 	else 
 		original(thisptr, previewmodel);
 }
