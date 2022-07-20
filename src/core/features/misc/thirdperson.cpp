bdc8bb1975f5b7db5e90543f5244b2cfddc9c9b7
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-07-20 21:48:08 +0200

    Reorganize thirdperson a bit

--- src/core/features/misc/thirdperson.cpp
+++ src/core/features/misc/thirdperson.cpp
@@ -1,15 +1,17 @@
 #include "dependencies/utilities/csgo.hpp"
 #include "core/features/features.hpp"
+#include "core/helpers/misc_helpers.hpp"
 #include "core/menu/variables.hpp"
 
 void misc::thirdperson() {
 	if (!interfaces::engine->is_connected() || !interfaces::engine->is_in_game()) return;
-	if (!csgo::local_player) return;
-    if (interfaces::engine->is_taking_screenshot() && variables::misc::clean_screenshots) {
-        reset_thirdperson();
-        return;
-    }
-    if (!csgo::local_player->is_alive() || !variables::misc::thirdperson) {
+    
+    // TODO: Thirdperson on spectated player
+    player_t* player = csgo::local_player;
+    if (!variables::misc::thirdperson
+        || !player
+        || !player->is_alive()
+        || (interfaces::engine->is_taking_screenshot() && variables::misc::clean_screenshots)) {
         reset_thirdperson();
         return;
     }
@@ -17,18 +19,20 @@ void misc::thirdperson() {
     static bool thirdperson_toggled = true;
     if (input::gobal_input.IsPressed(variables::misc::thirdperson_key))
         thirdperson_toggled = !thirdperson_toggled;
+
     if (!thirdperson_toggled) {
         reset_thirdperson();
         return;
     }
 
-    weapon_t* active_weapon = csgo::local_player->active_weapon();
-    if (!active_weapon) return;
-    const auto weapon_data = active_weapon->get_weapon_data();
-    if (!weapon_data) return;
-    if (weapon_data->weapon_type == WEAPONTYPE_GRENADE && (input::gobal_input.IsHeld(VK_LBUTTON) || input::gobal_input.IsHeld(VK_RBUTTON))) {
-        reset_thirdperson();
-        return;
+    // If we are alive and we are trying to throw a grenade, reset thirdperson
+    if (csgo::local_player && csgo::local_player->is_alive()) {
+        weapon_t* active_weapon = csgo::local_player->active_weapon();
+        if (!active_weapon) return;
+        if (active_weapon->is_grenade() && (input::gobal_input.IsHeld(VK_LBUTTON) || input::gobal_input.IsHeld(VK_RBUTTON))) {
+            reset_thirdperson();
+            return;
+        }
     }
 
     float distance = variables::misc::thirdperson_dist + 5.f;
@@ -38,13 +42,13 @@ void misc::thirdperson() {
     view_angles.z = 0.f;
 
     vec3_t forward(math::angle_vector(view_angles));
-    vec3_t eyes = csgo::local_player->get_eye_pos();
+    vec3_t eyes = player->get_eye_pos();
 
     ray_t ray;
     ray.initialize(eyes, (eyes - forward * distance));
         
     trace_filter filter;
-    filter.skip = csgo::local_player;
+    filter.skip = player;
 
     trace_t trace;
     interfaces::trace_ray->trace_ray(ray, MASK_SOLID, &filter, &trace);     // Detect hit behind (make distance smaller)
