b2f4ac089bee5007242f0cd58fd16b9dd1c2e5b7
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-07-26 21:17:59 +0200

    Reset worldcolor when disabling var

--- src/core/features/visuals/worldcolor.cpp
+++ src/core/features/visuals/worldcolor.cpp
@@ -2,17 +2,30 @@
 #include "core/features/features.hpp"
 #include "core/menu/variables.hpp"
 
-void visuals::worldcolor() {
-	if (!variables::misc_visuals::worldcolor) return;
-    if (!interfaces::engine->is_connected() || !interfaces::engine->is_in_game()) return;
-
+inline void apply_worldcolor(color col) {
     for (material_handle_t handle = interfaces::material_system->first_material(); handle != interfaces::material_system->invalid_material_handle(); handle = interfaces::material_system->next_material(handle)) {
         i_material* mat = interfaces::material_system->get_material(handle);
-        if (!mat || !mat->is_precached()) continue;     // @todo: crash
+        if (!mat || !mat->is_precached()) continue;
 
         const std::string_view texture_group = mat->get_texture_group_name();
 
         if (texture_group.starts_with("World") || texture_group.starts_with("StaticProp"))
-            mat->color_modulate(variables::colors::worldcolor_c.col.r / 255.f, variables::colors::worldcolor_c.col.g / 255.f, variables::colors::worldcolor_c.col.b / 255.f);
+            mat->color_modulate(col.r / 255.f, col.g / 255.f, col.b / 255.f);
     }
+}
+
+void visuals::worldcolor() {
+    static bool using_worldcolor = false;       // If true it means we just used worldcolor, so if we disable the menu var, it will apply default worldcolor to reset 
+
+    if (!variables::misc_visuals::worldcolor) {
+        if (using_worldcolor) {
+            apply_worldcolor(color::white());
+            using_worldcolor = false;
+        }
+        return;
+    }
+    if (!interfaces::engine->is_connected() || !interfaces::engine->is_in_game()) return;
+
+    using_worldcolor = true;
+    apply_worldcolor(variables::colors::worldcolor_c.col);
 }
