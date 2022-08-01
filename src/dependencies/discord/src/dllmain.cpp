593ee037bdb9d2256cb575bce3fd4ad511df882c
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-08-02 00:52:40 +0200

    Add discord rich presence dependency

---
+++ src/dependencies/discord/src/dllmain.cpp
@@ -0,0 +1,11 @@
+#include "dependencies/utilities/csgo.hpp"
+#include <windows.h>
+
+/*
+// outsmart GCC's missing-declarations warning
+BOOL WINAPI DllMain(HMODULE, DWORD, LPVOID);
+BOOL WINAPI DllMain(HMODULE, DWORD, LPVOID)
+{
+    return TRUE;
+}
+*/
