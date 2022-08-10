3e9fa7b8f6698cef3785a557a6c217ad8783ba51
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-08-10 03:34:20 +0200

    README - Add rank revealer

--- src/core/features/misc/rank_revealer.cpp
+++ src/core/features/misc/rank_revealer.cpp
@@ -3,8 +3,6 @@
 #include "core/menu/variables.hpp"
 
 void misc::reveal_ranks(c_usercmd* cmd) {
-    if (!variables::misc::reveal_ranks) return;
-    if (!(cmd->buttons & in_score)) return;
-
-    interfaces::client->dispatch_user_message(50, 0, 0, nullptr);
+    if (variables::misc::reveal_ranks && cmd->buttons & in_score) 
+        interfaces::client->dispatch_user_message(50, 0, 0, nullptr);
 }
